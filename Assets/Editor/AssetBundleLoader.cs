6d9bdcc033ca07dc5024d82e9be72a889ea98f62
Vanaheymr <dionon32@gmail.com>
2022-08-20 01:56:07 +0300

    Added Unity project

---
+++ Assets/Editor/AssetBundleLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class AssetBundleLoader
+{
+    [MenuItem("Tools/View AssetBundle")]
+    public static void LoadAndViewAssetBundle()
+    {
+        var path = EditorUtility.OpenFilePanel("Select AssetBundle", "", "");
+        if (!string.IsNullOrEmpty(path))
+        {
+            var assetBundle = AssetBundle.LoadFromFile(path);
+            Debug.Log($"AssetBundle {assetBundle.name}:\n{string.Join("\n", assetBundle.GetAllAssetNames())}");
+
+            var root = new GameObject(assetBundle.name);
+            foreach (var asset in assetBundle.LoadAllAssets())
+            {
+                if (asset is GameObject)
+                {
+                    Object.Instantiate(asset, root.transform);
+                }
+            }
+            assetBundle.Unload(false);
+        }
+    }
+}
---
+++ Assets/Editor/ContentBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class ContentBuilder
+{
+    private static readonly string PackageFolder = Path.Combine(Path.GetDirectoryName(Application.dataPath), "Package");
+
+    [MenuItem("Tools/Build AssetBundles", priority = 2)]
+    public static void BuildAssetBundles()
+    {
+        var buildPath = Path.Combine(PackageFolder, "AssetBundles");
+        ClearTargetDirectory(buildPath);
+
+        BuildPipeline.BuildAssetBundles(buildPath, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows);
+
+        var directoryName = Path.GetFileNameWithoutExtension(buildPath);
+        var buildBundlePath = Path.Combine(buildPath, $"{directoryName}");
+        var buildBundleManifestPath = $"{buildBundlePath}.manifest";
+
+        var filesToDelete = new List<string>();
+        filesToDelete.Add(buildBundlePath);
+        filesToDelete.Add(buildBundleManifestPath);
+
+        foreach (var assetBundleName in AssetDatabase.GetAllAssetBundleNames())
+        {
+            var bundlePath = Path.Combine(buildPath, assetBundleName);
+            var manifestPath = $"{bundlePath}.manifest";
+            filesToDelete.Add(manifestPath);
+
+            if (File.Exists(bundlePath))
+                File.Move(bundlePath, $"{bundlePath}.assets");
+            else
+                Debug.LogWarning($"Cannot find assetbundle {assetBundleName}");
+        }
+
+        filesToDelete.ForEach(path =>
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+        });
+
+        AssetDatabase.Refresh();
+
+        Debug.Log($"AssetBundles built successfully: {buildPath}\nAssetBundles: {string.Join(", ", AssetDatabase.GetAllAssetBundleNames())}");
+    }
+
+    private static void ClearTargetDirectory(string path)
+    {
+        if (Directory.Exists(path))
+            Directory.Delete(path, true);
+
+        Directory.CreateDirectory(path);
+    }
+}
